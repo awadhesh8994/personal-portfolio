c21bc8e785a8f7412b4a3a60179bf66d7d3fa70a
Awadhesh Kumar <awadhesh23th@gmail.com>
2025-03-02 13:51:43 +0530

    Update visitors-counter.js

--- visitors-counter.js
+++ visitors-counter.js
@@ -1,6 +1,6 @@
-fetch('https://api.countapi.xyz/hit/awadhesh-online.vercel.app/visits')
-  .then(response => response.json())
-  .then(data => {
-    document.getElementById('visitor-count').innerText = data.value;
-  })
-  .catch(error => console.error('Error fetching visitor count:', error));
+// fetch('https://api.countapi.xyz/hit/awadhesh-online.vercel.app/visits')
+//   .then(response => response.json())
+//   .then(data => {
+//     document.getElementById('visitor-count').innerText = data.value;
+//   })
+//   .catch(error => console.error('Error fetching visitor count:', error));
