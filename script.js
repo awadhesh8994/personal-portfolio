172474669dab914e599cbb00ca3a20ef1f2ca209
Awadhesh Kumar <awadhesh23th@gmail.com>
2025-03-02 13:43:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
-document.addEventListener("DOMContentLoaded", function () {
-  fetch('https://api.countapi.xyz/hit/awadhesh-online.vercel.app/visits')
-    .then(response => response.json())
-    .then(data => {
-      document.getElementById('visitor-count').innerText = data.value;
-    })
-    .catch(error => console.error('Error fetching visitor count:', error));
-});
